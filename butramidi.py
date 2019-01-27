4f15815bbb4d5b2ff9a1c1db1df6dfa5f82273c7
Gaizka DC <gaizka@mbpretina.yucos.local>
2019-01-27 10:35:32 +0100

    minor cleanup and update

--- butramidi.py
+++ butramidi.py
@@ -1,20 +1,23 @@
 #!/usr/local/bin/python
 
+# Imports
 from midiutil import MIDIFile
 from random import randint
 import random
 
-additional_tracks = randint (0,2)
+# Variables initialization
+additional_tracks = randint (0,4)
 print (str (additional_tracks))
 MyMIDI = MIDIFile(4+additional_tracks)
-
 channel  = 0
+track = 0
 time     = 0    # In beats
 duration = 0.25    # In beats
 tempo    = 120   # In BPM
 volume   = 80  # 0-127, as per the MIDI standard
 
-def create_beat (degrees, track):
+# Functions
+def create_beat (degrees):
     resolution = len (degrees) / 4
     MyMIDI.addTempo(track, time, tempo)
     for i, pitch in enumerate(degrees):
@@ -24,55 +27,40 @@ def create_beat (degrees, track):
         else:
             MyMIDI.addNote(track, channel, pitch, position, duration, volume)
 
-def create_random_beat (track):
-    x = randint (36, 61)
+def create_random_beat ():
+    x = randint (36, 52)
     print (str (x))
     degrees  = [0, 0, 0, 0, 0, 0, 0, 0]
     for i in range (0, 8):
         if bool(random.getrandbits(1)):
             degrees [i] = x
     print (degrees)
-    create_beat (degrees, track)
+    create_beat (degrees)
 
 # Kick
-
 x = 36
 degrees  = [x, 0, x, 0, x, 0, x, 0]  # MIDI note number
-track    = 0
-create_beat (degrees, track)
+create_beat (degrees)
 
 # Open Hi-Hat
-
 x = 46
 degrees  = [0, x, 0, x, 0, x, 0, x]  # MIDI note number
-track    = 1
-
-create_beat (degrees, track)
+create_beat (degrees)
 
 # Closed Hi-Hat
-
 x = 42
 degrees  = [0, 0, 0, x, 0, 0, 0, x, 0, 0, 0, x, 0, 0, 0, x]  # MIDI note number
-track    = 2
-
-create_beat (degrees, track)
+create_beat (degrees)
 
 # Clap
-
 x = 39
 degrees  = [0, 0, x, 0, 0, 0, x, 0]  # MIDI note number
-track    = 3
-
-create_beat (degrees, track)
+create_beat (degrees)
 
 # Additional tracks
-if additional_tracks == 0:
-    pass
-elif additional_tracks == 1:
-    create_random_beat (4)
-else:
-    for i in [0, 1]:
-        create_random_beat (4+i)
+for i in range (0, additional_tracks):
+    create_random_beat ()
 
+# Save file
 with open("house-beat.mid", "wb") as output_file:
     MyMIDI.writeFile(output_file)
